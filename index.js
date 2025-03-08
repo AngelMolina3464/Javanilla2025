ed8527a56576f240bb2b13a93401800c27d5e691
molina <molina_gap@outlook.es>
2025-03-08 17:34:22 -0600

    Con la Nueva Pagina

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 5000; 
+const port = 4800; 
 
 // Para Archivos Estaticos con Express
 app.use(express.static(path.join(__dirname, 'public')));
--- router/mainRouters.js
+++ router/mainRouters.js
@@ -3,18 +3,22 @@ import path from "path"
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename);;
 
 const mainRouter = express.Router(); 
-
+const urlpath = path.join(__dirname, 'public', 'tech.html').replace("router\\", "")
+console.log(urlpath)
 mainRouter.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
 })
-mainRouter.get('/2', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+mainRouter.get('/tech', (req, res) => {
+    const urlpath = path.join(__dirname, 'public' , 'tech.html').replace("router\\", "")
+    res.sendFile(urlpath)
 })
-mainRouter.get('/3', (req, res) => {
-    res.send("Si jala 3");
+mainRouter.get('/abe', (req, res) => {
+    const urlpath = path.join(__dirname, 'public' , 'abe.html').replace("router\\", "")
+    res.sendFile(urlpath);
 })
 
 export default mainRouter
